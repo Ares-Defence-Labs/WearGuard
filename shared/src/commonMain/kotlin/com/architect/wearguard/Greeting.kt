a875dbf92f6589f819d5fcd5fed190f84936a498
TheArchitect123 <dan.developer789@gmail.com>
2025-08-26 15:54:36 +1000

    Initial Commit

---
+++ shared/src/commonMain/kotlin/com/architect/wearguard/Greeting.kt
@@ -0,0 +1,9 @@
+package com.architect.wearguard
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/architect/wearguard/Platform.kt
@@ -0,0 +1,7 @@
+package com.architect.wearguard
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
