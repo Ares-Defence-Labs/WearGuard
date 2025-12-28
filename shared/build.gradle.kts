a725a0c008bb4f14945425ef63cac382fa4e3b1f
TheArchitect123 <dan.developer789@gmail.com>
2025-12-28 15:10:30 +1100

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,14 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+
+    id("org.gradle.maven-publish")
+    id("signing")
+    id("com.vanniktech.maven.publish") version "0.29.0"
+    id("kotlin-parcelize")
 }
 
 kotlin {
@@ -75,4 +81,62 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+//////////////////////////////
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.thearchitect123",
+        artifactId = "wear-guard",
+        version = "1.0.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("WearGuard")
+        description.set("WearGuard is a KMP Library for secure and customisable communication between wearable devices and mobile devices")
+        inceptionYear.set("2024")
+        url.set("https://github.com/Ares-Defence-Labs/WearGuard")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("Dan Gerchcovich")
+                name.set("TheArchitect123")
+                email.set("dan.developer789@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/Ares-Defence-Labs/WearGuard")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
+
+signing {
+    val privateKeyFile = project.findProperty("signing.privateKeyFile") as? String
+        ?: error("No Private key file found")
+    val passphrase = project.findProperty("signing.password") as? String
+        ?: error("No Passphrase found for signing")
+
+    // Read the private key from the file
+    val privateKey = File(privateKeyFile).readText(Charsets.UTF_8)
+
+    useInMemoryPgpKeys(privateKey, passphrase)
+    sign(publishing.publications)
 }
